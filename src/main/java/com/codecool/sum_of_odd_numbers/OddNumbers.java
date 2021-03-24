b2e098ab4e1f513c893432485b4940c8fa2ae7a4
vlad <vlad@localhost.localdomain>
2021-03-24 15:39:40 +0100

    Tests passed

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -1,10 +1,18 @@
 package com.codecool.sum_of_odd_numbers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
-        return null;
+
+        Integer sum = 0;
+        for (Integer number : numbers) {
+            if (number % 2 != 0) {
+                sum += number;
+            }
+        }
+        return sum;
     }
 }
